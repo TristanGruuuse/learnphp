3a9686c4f440736e1c3b7a813f3e49c830fab131
Tristan Kenneth Gruuse <Tristankenneth.gruuse@tptlive.ee>
2025-09-04 10:54:03 +0300

    num

--- index.php
+++ index.php
@@ -1,60 +1,26 @@
 <?php
 
-$array = array(1, 2, 3);
-var_dump($array);
-$array = [1, 2, 3];
-var_dump($array);
-$array = [
-    1,
-    'tristan',
-    true,
-    [1, 2, 3],
-    3.4,
-];
+$num = 1;
+$num = $num + 1;
+var_dump($num);
+$num = $num - 1;
+var_dump($num);
+$num = $num * 2;
+var_dump($num);
+$num = $num / 2;
+var_dump($num);
+$num = $num % 2;
+var_dump($num);
+$num += 1;
+$num -= 1;
+$num *= 2;
+$num /= 2;
+$num %= 2;
+var_dump($num);
+$num++;
+$num--;
+var_dump($num);
+$num = $num ** 2;
+$num **= 2;
 
-var_dump($array);
-$array = [
-    'name' => 'Tristan',
-    'age' => 18,
-    'isMale' => true,
-];
 
-var_dump($array);$array = [
-    1,
-    2,
-    'name' => 'Tristan',
-    'age' => 18,
-    6,
-    'isMale' => true,
-    100 => 3,
-    4,
-    5,
-];
-var_dump($array);
-
-$array = [1, 2, 3];
-
-var_dump($array[1]);
-
-var_dump($array);
-$array = [
-    'name' => 'Tristan',
-    'age' => 18,
-    'isMale' => true,
-];
-var_dump($array['name']);
-$array = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-var_dump($array[2][1]);
-
-$array = [1,2,3];
-var_dump($array);
-array_push($array, 4);
-array_push($array, 5,6,7);
-$array[] = 8;
-var_dump($array);
-$array[1] ='something else';
-var_dump($array);
