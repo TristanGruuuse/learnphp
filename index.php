b19d4643d9e4fb0db3af7650111cd51493738376
Tristan Kenneth Gruuse <Tristankenneth.gruuse@tptlive.ee>
2025-09-04 11:45:05 +0300

    Day

--- index.php
+++ index.php
@@ -1,64 +1,54 @@
 <?php
+$num = 10;
+if ($num > 5) {
+    var_dump('Bigger then 5');
+} elseif ($num === 5){
+var_dump('Equal to 5');
+} else {
+    var_dump('Smaller then 5');
+}
 
-$bool = true && false;
+$day = (int) date('N', strtotime('07.09.2025'));
+var_dump($day);
 
-var_dump($bool);
+if ($day == 1){
+    var_dump ('Esmaspäev');
+}else if ($day == 1){
+    var_dump ('Teisipäev');
+}
+else if ($day == 1){
+    var_dump ('Kolmapäev');
+}
+else if ($day == 1){
+    var_dump ('neljapäev');
+}
+else if ($day == 1){
+    var_dump ('Reede');
+}
+else if ($day == 1){
+    var_dump ('Laupäev');
+}
+else if ($day == 1){
+    var_dump ('Pühapäev');
+}
 
-$bool = true || false;
+switch($day){
+    case 1:
+        var_dump ('Esmaspäev');
+    case 2:
+        var_dump ('Teisipäev');
+    case 3:
+        var_dump ('Kolmapäev');
+    case 4:
+        var_dump ('neljapäev');
+    case 5:
+        var_dump ('Reede');
+    case 6:
+        var_dump ('Laupäev');
+    case 7:
+        var_dump ('Pühapäev');
 
-var_dump ($bool);
-
-$bool = !true;
-
-var_dump($bool);
-
-$bool = !false;
-
-var_dump($bool);
-
-$bool = 10 > 5;
-
-var_dump($bool);
-
-$bool = 10 < 5;
-
-var_dump($bool);
-
-$bool = 10 < 10;
-
-var_dump($bool);
-
-$bool = 10 > 10;
-
-var_dump($bool);
-
-$bool = 10 == 10;
-
-var_dump($bool);
-
-$bool = 10 >= 10;
-
-var_dump($bool);
-
-$bool = 10 <= 10;
-
-var_dump($bool);
-
-$bool = 10 == '10';
-
-var_dump($bool);
-
-$bool = 10 === '10';
-
-var_dump($bool);
-
-$bool = 10 != 10;
-
-$bool = 10 !== 10;
-
-$bool = (true && false || true && !false) && true;
-
-var_dump($bool);
+}
 
 
 
